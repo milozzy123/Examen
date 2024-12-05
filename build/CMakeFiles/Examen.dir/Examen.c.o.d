f1a49ef3b2fd77418a1bc7c989b6c083d4dc6193
milo geullaume <milozzy123@gmail.com>
2024-12-05 14:02:16 +0100

    ez

---
+++ build/CMakeFiles/Examen.dir/Examen.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/Examen.dir/Examen.c.o: /home/milo/examen/Examen.c \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/Examen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen.dir/Examen.c.o"
+  "CMakeFiles/Examen.dir/Examen.c.o.d"
+  "Examen"
+  "Examen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Examen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
